9cc1816c3e9edffa3924e93b349b563ef27801e2
ZaraMansoor <zaramansoor2@gmail.com>
2025-02-23 21:02:19 -0500

    cahnged typo

--- rpi/main.py
+++ rpi/main.py
@@ -18,7 +18,7 @@ while True:
     temperature_f = temperature_c * (9 / 5) + 32
     humidity = dht_device.humidity
 
-    print("Temp:{:.1f} C / {:.1f} C    Humidity: {}%".format(temperature_c, temperature_f, humidity))
+    print("Temp:{:.1f} C / {:.1f} F    Humidity: {}%".format(temperature_c, temperature_f, humidity))
   except RuntimeError as err:
     print(err.args[0])
 
