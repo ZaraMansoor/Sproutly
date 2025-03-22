2da79ef02f0435f05f959717e3b8a3626621f5a8
jarmouti <jarmouti@andrew.cmu.edu>
2025-03-22 15:39:23 -0400

    Updated plant identification to avoid storing images

--- rpi/plant_id_api.py
+++ rpi/plant_id_api.py
@@ -7,36 +7,39 @@ Using Pl@ntNet API
 
 import requests
 import json
+import io
 from pprint import pprint
 from picamera2 import Picamera2
 
-API_KEY = "2b10KGTzAgqcb5baVQciCheRU"  # Set you API_KEY here
-PROJECT = "all" # try "weurope" or "canada"
+API_KEY = "2b10KGTzAgqcb5baVQciCheRU"  # Set your API_KEY here
+PROJECT = "all"  # try "weurope" or "canada"
 api_endpoint = f"https://my-api.plantnet.org/v2/identify/{PROJECT}?api-key={API_KEY}"
 
 def identify_plant():
-  picam2 = Picamera2()
-  picam2.start_and_capture_file("data/test.jpg")
-  image_path_1 = "data/test.jpg"
-  image_data_1 = open(image_path_1, 'rb')
+    picam2 = Picamera2()
+    picam2.start()
+    
+    # capture image into memory
+    image_stream = io.BytesIO()
+    picam2.capture_file(image_stream, format="jpeg")
+    image_stream.seek(0)
 
-  files = [
-      ('images', (image_path_1, image_data_1))
-  ]
+    files = [
+        ('images', ('image.jpg', image_stream, 'image/jpeg'))
+    ]
 
-  req = requests.Request('POST', url=api_endpoint, files=files, data={})
-  prepared = req.prepare()
+    response = requests.post(api_endpoint, files=files)
 
-  s = requests.Session()
-  response = s.send(prepared)
-  json_result = json.loads(response.text)
+    json_result = response.json()
 
-  pprint(response.status_code)
-  pprint(json_result)
+    pprint(response.status_code)
+    pprint(json_result)
 
-  if (response.status_code == 200):
-    best_match = json_result["bestMatch"]
-    common_names = json_result["results"][0]["species"]["commonNames"]
-    print(f"best match: {best_match}, common names: {common_names}")
+    if response.status_code == 200:
+        best_match = json_result.get("bestMatch", "Unknown")
+        common_names = json_result["results"][0]["species"].get("commonNames", [])
+        print(f"Best match: {best_match}, Common names: {common_names}")
 
-    return best_match, common_names
+        return best_match, common_names
+
+identify_plant()
