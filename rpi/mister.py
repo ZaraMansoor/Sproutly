da7cf8d7a3badc9efb20731dd19055e81d645cd7
jarmouti <jarmouti@andrew.cmu.edu>
2025-03-29 16:08:32 -0400

    mister  code

---
+++ rpi/mister.py
@@ -0,0 +1,18 @@
+import os
+import time
+
+USB_PORT_PATH = "/sys/bus/usb/devices/usb1/power/control"
+
+def turn_on():
+    with open(USB_PORT_PATH, "w") as f:
+        f.write("on")
+    print("Mister ON")
+
+def turn_off():
+    with open(USB_PORT_PATH, "w") as f:
+        f.write("auto")
+    print("Mister OFF")
+
+turn_on()
+time.sleep(5)
+turn_off()
