161e9255d369a098d7c6d22c6283dd9283d03e81
jarmouti <jarmouti@andrew.cmu.edu>
2025-03-29 13:49:05 -0400

    buggg

--- rpi/light.py
+++ rpi/light.py
@@ -12,14 +12,14 @@ class Relay(OutputDevice):
     def __init__(self, pin, active_high=False):
         super().__init__(pin, active_high=active_high)
 
-# using GPIO pin 12
-light = Relay(12, active_high=False)
+# using GPIO pin 18
+light = Relay(18, active_high=False)
 
 while True:
     light.on()
     print("LED ON")
-    sleep(5)
+    sleep(10)
     
     light.off()
     print("LED OFF")
-    sleep(5)
+    sleep(10)
