9179d0fb69ab471699349bf075cfb4c76df0c0c4
ZaraMansoor <zaramansoor2@gmail.com>
2025-03-29 22:31:30 -0400

    edited to run

--- rpi/arduino_comm.py
+++ rpi/arduino_comm.py
@@ -3,9 +3,10 @@ import serial
 
 if __name__ == '__main__':
   ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
+  time.sleep(2)
   ser.reset_input_buffer()
 
   while True:
     if ser.in_waiting > 0:
-      line = ser.readline().decode('utf-8')
+      line = ser.readline().decode('utf-8').strip()
       print(line)
