c743bb28cd8191ddb9fd3e8eaeb2a46e9d0dc2de
ZaraMansoor <zaramansoor2@gmail.com>
2025-03-31 11:55:46 -0400

    light code

--- rpi/light.py
+++ rpi/light.py
@@ -12,14 +12,39 @@ class Relay(OutputDevice):
     def __init__(self, pin, active_high=False):
         super().__init__(pin, active_high=active_high)
 
-# using GPIO pin 24
-light = Relay(24, active_high=False)
+# using GPIO pins 26 6 5
+light_1 = Relay(26, active_high=False)
+light_2 = Relay(6, active_high=False)
+light_3 = Relay(5, active_high=False)
 
 while True:
-    light.on()
-    print("LED ON")
-    sleep(5)
-    
-    light.off()
-    print("LED OFF")
-    sleep(5)
+    user_input = input("how many lights? (0, 1, 2 or 3) ").strip().lower()
+
+    if user_input == '0':
+        light_1.on()
+        light_2.on()
+        light_3.on()
+        print("0 lights are on")
+    elif user_input == '1':
+        light_1.off()
+        light_2.on()
+        light_3.on()
+        print("1 light is on")
+    elif user_input == '2':
+        light_1.off()
+        light_2.off()
+        light_3.on()
+        print("2 lights are on")
+    elif user_input == '3':
+        light_1.off()
+        light_2.off()
+        light_3.off()
+        print("3 lights are on")
+    elif user_input == 'exit':
+        print("Exiting program...")
+        break
+    else:
+        print("Invalid input. Please type '0', 1', '2', or '3'.")
+  
+    time.sleep(1)
+
