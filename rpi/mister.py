f32ab27cbbc53cc167d113e951d23dc25b9bacd8
jarmouti <jarmouti@andrew.cmu.edu>
2025-04-02 12:40:31 -0400

    mister

--- rpi/mister.py
+++ rpi/mister.py
@@ -1,19 +1,27 @@
-import os
+import RPi.GPIO as GPIO
 import time
 
-USB_PORT_PATH = "/sys/bus/usb/devices/usb1/power/control"
+MISTER_PIN = 12
 
-def turn_on():
-    with open(USB_PORT_PATH, "w") as f:
-        f.write("on")
-    print("Mister ON")
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(MISTER_PIN, GPIO.OUT)
 
-def turn_off():
-    with open(USB_PORT_PATH, "w") as f:
-        f.write("auto")
-    print("Mister OFF")
+def press_mister(press_count=1, delay_between=0.5):
+    """Simulates pressing the mister button press_count times."""
+    for _ in range(press_count):
+        GPIO.output(MISTER_PIN, GPIO.HIGH)  # Close relay (press button)
+        time.sleep(0.2)  # Duration of button press
+        GPIO.output(MISTER_PIN, GPIO.LOW)   # Release relay
+        if _ < press_count - 1:
+            time.sleep(delay_between)  # Wait before next press
 
-turn_on()
-time.sleep(10)
-turn_off()
-time.sleep(10)
+try:
+    while True:
+        user_input = input("Enter number of presses (1-3): ")
+        if user_input in ["1", "2", "3"]:
+            press_mister(int(user_input))
+        else:
+            print("Invalid input. Enter 1, 2, or 3.")
+
+except KeyboardInterrupt:
+    GPIO.cleanup()
