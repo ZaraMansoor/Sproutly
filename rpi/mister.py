f1c29b74adfcb3ce951be6a4d9db921b93524eb1
jarmouti <jarmouti@andrew.cmu.edu>
2025-04-02 12:43:54 -0400

    mister

--- rpi/mister.py
+++ rpi/mister.py
@@ -1,27 +1,26 @@
-import RPi.GPIO as GPIO
+from gpiozero import OutputDevice
 import time
 
-MISTER_PIN = 12
+class Relay(OutputDevice):
+    def __init__(self, pin, active_high=False):
+        super().__init__(pin, active_high=active_high)
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(MISTER_PIN, GPIO.OUT)
+# using GPIO pin 12
+mister = Relay(12, active_high=False)
+
 
 def press_mister(press_count=1, delay_between=0.5):
     """Simulates pressing the mister button press_count times."""
     for _ in range(press_count):
-        GPIO.output(MISTER_PIN, GPIO.HIGH)  # Close relay (press button)
+        mister.on()  # Close relay (press button)
         time.sleep(0.2)  # Duration of button press
-        GPIO.output(MISTER_PIN, GPIO.LOW)   # Release relay
+        mister.off()   # Release relay
         if _ < press_count - 1:
             time.sleep(delay_between)  # Wait before next press
 
-try:
-    while True:
-        user_input = input("Enter number of presses (1-3): ")
-        if user_input in ["1", "2", "3"]:
-            press_mister(int(user_input))
-        else:
-            print("Invalid input. Enter 1, 2, or 3.")
-
-except KeyboardInterrupt:
-    GPIO.cleanup()
+while True:
+    user_input = input("Enter number of presses (1-3): ")
+    if user_input in ["1", "2", "3"]:
+        press_mister(int(user_input))
+    else:
+        print("Invalid input. Enter 1, 2, or 3.")
