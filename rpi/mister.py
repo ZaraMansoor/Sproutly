3466939b8edc4670522abd80be6d1ca3c7fc1080
jarmouti <jarmouti@andrew.cmu.edu>
2025-04-02 13:16:02 -0400

    mister pls

--- rpi/mister.py
+++ rpi/mister.py
@@ -12,15 +12,25 @@ mister = Relay(12, active_high=False)
 def press_mister(press_count=1, delay_between=0.5):
     """Simulates pressing the mister button press_count times."""
     for _ in range(press_count):
-        mister.on()  # Close relay (press button)
-        time.sleep(0.2)  # Duration of button press
-        mister.off()   # Release relay
+        mister.on()
+        time.sleep(0.2)
+        mister.off()
         if _ < press_count - 1:
-            time.sleep(delay_between)  # Wait before next press
+            time.sleep(delay_between)
+
+# while True:
+#     user_input = input("Enter number of presses (1-3): ")
+#     if user_input in ["1", "2", "3"]:
+#         press_mister(int(user_input))
+#     else:
+#         print("Invalid input. Enter 1, 2, or 3.")
+
 
 while True:
     user_input = input("Enter number of presses (1-3): ")
-    if user_input in ["1", "2", "3"]:
-        press_mister(int(user_input))
+    if user_input in ["on"]:
+        mister.on()
+    if user_input in ["off"]:
+        mister.off()
     else:
         print("Invalid input. Enter 1, 2, or 3.")
