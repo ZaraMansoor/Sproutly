d81c362c2bba83e601a93744e0be239d9476cd3f
Yuna Shin <yoonahs@andrew.cmu.edu>
2025-04-07 10:44:58 -0400

    change db settings

--- webapp/backend/webapps/settings.py
+++ webapp/backend/webapps/settings.py
@@ -87,8 +87,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'plantdata',
-        'USER': 'root',
-        'PASSWORD': '',
+        'USER': 'sproutly',
+        'PASSWORD': 'stanley',
         'HOST': 'localhost',
         'PORT': '3306',
     }
