14b5c70f18389c74ad3a4ab2f6588496e3fb3e5c
ZaraMansoor <zaramansoor2@gmail.com>
2025-04-09 12:08:24 -0400

    change temp

--- rpi/temp_hum_sensor.py
+++ rpi/temp_hum_sensor.py
@@ -1,14 +1,18 @@
-import pigpio
-import time
 import dht11
+import RPi.GPIO as GPIO
+import time
 
-pi = pigpio.pi()
-sensor = dht11.DHT11(pi, 17)  # GPIO17
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+GPIO.cleanup()
+
+sensor = dht11.DHT11(pin=17)
 
 while True:
-  result = sensor.read()
-  if result[0] == dht11.DHT11.OK:
-    print(f"Temp: {result[1]} C  Humidity: {result[2]}%")
-  else:
-    print("Failed to read.")
-  time.sleep(2)
+    result = sensor.read()
+    if result.is_valid():
+        print(f"Temperature: {result.temperature} C")
+        print(f"Humidity: {result.humidity} %")
+    else:
+        print("Failed to read from DHT11")
+    time.sleep(2)
