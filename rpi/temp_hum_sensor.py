82b44dd9dcc278298f76baba8052f863e3a611f6
ZaraMansoor <zaramansoor2@gmail.com>
2025-04-09 11:52:46 -0400

    tester temp hum sensor

---
+++ rpi/temp_hum_sensor.py
@@ -0,0 +1,14 @@
+import pigpio
+import time
+import dht11
+
+pi = pigpio.pi()
+sensor = dht11.DHT11(pi, 17)  # GPIO17
+
+while True:
+  result = sensor.read()
+  if result[0] == dht11.DHT11.OK:
+    print(f"Temp: {result[1]} C  Humidity: {result[2]}%")
+  else:
+    print("Failed to read.")
+  time.sleep(2)
