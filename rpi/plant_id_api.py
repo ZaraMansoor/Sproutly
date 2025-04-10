41724d62a5d79c7e8d6fba9706e01d7ed4234869
jarmouti <jarmouti@andrew.cmu.edu>
2025-04-10 19:05:49 -0400

    removed debugging statements

--- rpi/plant_id_api.py
+++ rpi/plant_id_api.py
@@ -18,37 +18,11 @@ PROJECT = "all"  # try "weurope" or "canada"
 api_endpoint = f"https://my-api.plantnet.org/v2/identify/{PROJECT}?api-key={API_KEY}"
 
 def identify_plant(files):
-    # picam2 = Picamera2()
-    # picam2.start()
-    
-    # # capture image into memory
-    # image_stream = io.BytesIO()
-    # picam2.capture_file(image_stream, format="jpeg")
-    # image_stream.seek(0)
-    
-    # '''
-    # image = Image.open(image_stream)
-    # plt.imshow(image)
-    # plt.axis('off')
-    # plt.show()
-    # '''
-
-    # files = [
-    #     ('images', ('image.jpg', image_stream, 'image/jpeg'))
-    # ]
-
     response = requests.post(api_endpoint, files=files)
-
     json_result = response.json()
-
-    pprint(response.status_code)
-    pprint(json_result)
-
     if response.status_code == 200:
         best_match = json_result.get("bestMatch", "Unknown")
         common_names = json_result["results"][0]["species"].get("commonNames", [])
-        print(f"Best match: {best_match}, Common names: {common_names}")
-
         return best_match, common_names
 
 if __name__ == "__main__":
