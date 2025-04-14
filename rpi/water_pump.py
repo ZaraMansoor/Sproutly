a362285bf5682d615b6c8445cbb18f10f4797829
ZaraMansoor <zaramansoor2@gmail.com>
2025-04-13 22:06:15 -0400

    edited water pump to add nutrients pump

--- rpi/water_pump.py
+++ rpi/water_pump.py
@@ -6,19 +6,39 @@ Zara Mansoor (zmansoor)
 from gpiozero import OutputDevice
 import time
 
-RELAY_PIN = 18
-relay = OutputDevice(RELAY_PIN)
-relay.off()
+class Relay(OutputDevice):
+  def __init__(self, pin, active_high=False):
+    super().__init__(pin, active_high=active_high)
+
+# water pump
+WATER_PUMP_RELAY_PIN = 18
+water_pump_relay = Relay(WATER_PUMP_RELAY_PIN, active_high=False)
+water_pump_relay.off()
+
+NUTRIENTS_PUMP_RELAY_PIN = 24
+nutrients_pump_relay = Relay(NUTRIENTS_PUMP_RELAY_PIN, active_high=False)
+nutrients_pump_relay.off()
+
 
 while True:
-  user_input = input("on or off? ").strip().lower()
+  user_input = input("0 = all off; 1 = water pump on; 2 = nutrients pump on; 3 = both pumps on").strip().lower()
 
-  if user_input == 'on':
-    relay.on()
+  if user_input == '0':
+    water_pump_relay.off()
+    nutrients_pump_relay.off()
+    print("all OFF")
+  elif user_input == '1':
+    water_pump_relay.on()
+    nutrients_pump_relay.off()
     print("water pump is ON")
-  elif user_input == 'off':
-    relay.off()
-    print("water pump is OFF")
+  elif user_input == '2':
+    water_pump_relay.off()
+    nutrients_pump_relay.on()
+    print("nutrients pump is ON")
+  elif user_input == '3':
+    water_pump_relay.on()
+    nutrients_pump_relay.on()
+    print("all ON")
   elif user_input == 'exit':
     print("Exiting program...")
     break
