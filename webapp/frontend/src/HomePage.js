c2e9a6f8ea9d5f1f8ac193da15c3d36c2d754dfe
Yuna Shin <yoonahs@andrew.cmu.edu>
2025-04-16 18:34:36 -0400

    import form

--- webapp/frontend/src/HomePage.js
+++ webapp/frontend/src/HomePage.js
@@ -14,6 +14,7 @@ import { useNavigate } from 'react-router-dom';
 import { Tab, Tabs } from 'react-bootstrap';
 import socket from './socket';
 import { Line } from 'react-chartjs-2';
+import { Form } from 'react-bootstrap';
 
 import {
     Chart as ChartJS,
