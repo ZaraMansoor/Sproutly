6df315d9b18b820d9d285670af8dd8a73e2015ba
ZaraMansoor <zaramansoor2@gmail.com>
2025-04-19 19:16:27 -0400

    trying mister

--- rpi/mister.py
+++ rpi/mister.py
@@ -25,14 +25,39 @@ def pulse():
     GPIO.output(HUMIDIFIER_PIN_1, GPIO.LOW)
     GPIO.output(HUMIDIFIER_PIN_2, GPIO.LOW)
 
-try:
-    time.sleep(2)
-    while True:
-        print('turning on')
-        pulse_l()
-        time.sleep(5)
-        print('turning off')
-        pulse_l()
-        time.sleep(5)
-except KeyboardInterrupt:
-    GPIO.cleanup()
+while True:
+    try:
+        user_input = input("0 = all off; 1 = mister 1 on; 2 = mister 2 on; 3 = both misters on").strip().lower()
+
+        if user_input == '0':
+            print("all OFF")
+        elif user_input == '1':
+            pulse_l()
+            print("water pump is ON")
+        elif user_input == '2':
+            pulse_r()
+            print("nutrients pump is ON")
+        elif user_input == '3':
+            pulse_l()
+            pulse_r()
+            print("all ON")
+        elif user_input == 'exit':
+            print("Exiting program...")
+            break
+        else:
+            print("Invalid input. Please type 'ON', 'OFF', or 'exit'.")
+    
+        time.sleep(1)
+
+    except KeyboardInterrupt:
+        GPIO.cleanup()
+
+    # time.sleep(2)
+    # while True:
+    #     print('turning on')
+    #     pulse_l()
+    #     time.sleep(5)
+    #     print('turning off')
+    #     pulse_l()
+    #     time.sleep(5)
+
