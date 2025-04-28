7ea811649a5bb5515ea8c2bd869563bbed0a697a
Yuna Shin <yoonahs@andrew.cmu.edu>
2025-04-28 14:08:44 -0400

    lower jpeg quality (just for now)

--- rpi/stream.py
+++ rpi/stream.py
@@ -26,7 +26,7 @@ import datetime
 # adjustable settings
 RESOLUTION = (3280, 2464)
 FRAME_RATE = 30
-JPEG_QUALITY = 100 # (1 - 100)
+JPEG_QUALITY = 80 # (1 - 100)
 
 # define daytime (bright light) settings
 DAY_SETTINGS = {
